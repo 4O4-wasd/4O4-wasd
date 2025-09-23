ed230e2a98a8190029bc34df6f7e8aa4bf2aeeaa
yuvu <notstoragestorage@gmail.com>
2025-09-23 20:35:52 +0530

    repo init

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+export default function Home() {
+    return (
+        <div className="flex flex-col items-center justify-center h-[100dvh]">
+            <h1
+                className="font-semibold text-9xl glitch layers font-[Geist] drop-shadow-sm drop-shadow-white"
+                data-text="४O४"
+            >
+                <span>4O4</span>
+            </h1>
+            <h2
+                className="mt-10 font-semibold text-4xl glitch layers font-[Geist] drop-shadow-xs drop-shadow-white"
+                data-text="जल्दी आ रहा हूँ"
+            >
+                <span>coming soon</span>
+            </h2>
+        </div>
+    );
+}
